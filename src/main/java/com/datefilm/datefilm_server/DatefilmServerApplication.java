cbe247bf884769d6c998922545e2f3cbc05a83bd
조성완 / dundun <rutc98@naver.com>
2024-06-27 17:51:44 +0900

    #2 feat: dot-env 세팅

--- src/main/java/com/datefilm/datefilm_server/DatefilmServerApplication.java
+++ src/main/java/com/datefilm/datefilm_server/DatefilmServerApplication.java
@@ -1,5 +1,6 @@
 package com.datefilm.datefilm_server;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
@@ -12,6 +13,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class DatefilmServerApplication {
 
     public static void main(String[] args) {
+
+        // .env 파일 로드
+        Dotenv dotenv = Dotenv.configure().load();
+        // 환경 변수를 시스템 속성으로 설정
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+
         SpringApplication.run(DatefilmServerApplication.class, args);
     }
 
